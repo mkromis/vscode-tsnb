5496ad71c3cc73944a579328924ff35a0dfbbcd9
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2024-06-08 16:24:51 -0400

    create sample serializer

---
+++ src/extension/tsnbSerializer.ts
@@ -0,0 +1,71 @@
+import { TextDecoder, TextEncoder } from "util";
+import * as vscode from "vscode";
+
+export interface RawNotebook {
+  cells: RawNotebookCell[];
+}
+
+export interface RawNotebookCell {
+  language: string;
+  source: string;
+  execution_count?: number;
+  outputs?: Output[];
+}
+
+export interface Output {
+  output_type: string;
+  data: Data;
+  execution_count: number;
+}
+
+export interface Data {
+  type: string; // such as "x-application/custom-json-output":
+  data: string; // hello world?
+}
+
+export interface Metadata2 {}
+
+export class tsnbSerializer implements vscode.NotebookSerializer {
+  async deserializeNotebook(
+    content: Uint8Array,
+    _token: vscode.CancellationToken
+  ): Promise<vscode.NotebookData> {
+    var contents = new TextDecoder().decode(content);
+
+    let raw: RawNotebookCell[];
+    try {
+      raw = (<RawNotebook>JSON.parse(contents)).cells;
+    } catch {
+      raw = [];
+    }
+
+    const cells = raw.map(
+      (item) =>
+        new vscode.NotebookCellData(
+          item.language === "markdown"
+            ? vscode.NotebookCellKind.Markup
+            : vscode.NotebookCellKind.Code,
+          item.source.toString(),
+          item.language === "code" ? "typescript" : "markdown"
+        )
+    );
+
+    return new vscode.NotebookData(cells);
+  }
+
+  async serializeNotebook(
+    data: vscode.NotebookData,
+    _token: vscode.CancellationToken
+  ): Promise<Uint8Array> {
+    let contents: RawNotebookCell[] = [];
+
+    for (const cell of data.cells) {
+      contents.push({
+        language: cell.languageId,
+        source: cell.value,
+      });
+    }
+
+    return new TextEncoder().encode(JSON.stringify(contents));
+  }
+}
